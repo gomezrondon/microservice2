3f51c5829c433c74b2f31422402a305130fef0cc
cajgomez <Jgomez@cambridgeassociates.com>
2020-03-18 20:40:28 -0600

    1rst commit Car-service

---
+++ car-service/src/main/java/com/gomezrondon/carservice/Application.java
@@ -0,0 +1,58 @@
+package com.gomezrondon.carservice;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	ApplicationRunner init(CarRepository repository) {
+		return args -> {
+			Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
+					"AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").map(String::trim).map(Car::new).forEach(repository::save);
+			repository.findAll().forEach(System.out::println);
+
+		};
+	}
+
+}
+
+@Data
+@NoArgsConstructor
+@Entity
+class Car {
+	@Id
+	@GeneratedValue
+	private Long id;
+	@NonNull
+	private String name;
+
+	public Car(String name) {
+		this.name = name;
+	}
+
+}
+
+@RepositoryRestResource
+interface CarRepository extends JpaRepository<Car, Long> {
+}
