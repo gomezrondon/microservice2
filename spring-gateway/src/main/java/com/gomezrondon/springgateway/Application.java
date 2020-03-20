0ef05088f84584a7d45d72d322d30fff7bf4b098
cajgomez <Jgomez@cambridgeassociates.com>
2020-03-20 12:38:40 -0600

    Calling a microservice by name.

--- spring-gateway/src/main/java/com/gomezrondon/springgateway/Application.java
+++ spring-gateway/src/main/java/com/gomezrondon/springgateway/Application.java
@@ -2,6 +2,7 @@ package com.gomezrondon.springgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -10,7 +11,7 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableHystrix
+@EnableCircuitBreaker
 public class Application {
 
 	public static void main(String[] args) {
@@ -21,19 +22,8 @@ public class Application {
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
-				.route(p-> p.path("/time") // esto funciona
-						.uri("http://time.jsontest.com/")
-				)
-				.route(p -> p.path("/all") // no funciona
-						.filters(f -> f.addRequestHeader("x-rapidapi-host","ajayakv-rest-countries-v1.p.rapidapi.com")
-						.addRequestHeader("x-rapidapi-key", "afacfffb4emsh85bd04fd60dcb05p182285jsn94cc737a7223")
-				)
-						.uri("https://ajayakv-rest-countries-v1.p.rapidapi.com/rest/v1")
-				)
-				.route(p -> p.path("/get") // si funciona
-						.filters(f -> f.addRequestHeader("hello", "world"))
-						.uri("http://httpbin.org:80")
-				)
+				.route("car-jpa-rest",r -> r.path("/**")
+				.uri("lb://car-service"))
 				.build();
 	}
 
